5efc73ee6c80a95c3a43c210503f7a0d5581192a
Allan Bismarck de Souza Santos <42256201+AllanBismarck123@users.noreply.github.com>
2022-05-08 22:16:14 -0300

    inputing part of main and details

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var showContent = false;
+
 function item_selected(index_item) {
     var div_itens = document.querySelectorAll("aside > nav > div > div");
     var p_itens = document.querySelectorAll("aside > nav > div > p");
@@ -9,4 +11,26 @@ function item_selected(index_item) {
     div_itens[index_item].style.width = "5px";
     div_itens[index_item].style.backgroundColor = "#F28941";
     p_itens[index_item].style.backgroundColor =  "#FDF1EB";
+}
+
+function show_content() {
+    showContent = !showContent;
+    var i;
+    var p = document.getElementsByClassName('see_more');
+    p_length = p.length;
+    if(showContent) {
+        document.getElementById('points').style.display = "none";
+        document.getElementById('show_see_more').style.display = "none";
+        document.getElementById('abstract').style.height = "540px";
+        for(i = 0; i < p_length; i ++) {
+            p[i].style.display = "block";
+        }
+    } else {
+        document.getElementById('points').style.display = "inline";
+        document.getElementById('show_see_more').style.display = "inline";
+        document.getElementById('abstract').style.height = "170px";
+        for(i = 0; i < p_length; i ++) {
+            p[i].style.display = "none";
+        }
+    }
 }
